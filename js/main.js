3d776956a9c088533c751afb986c2efc9fd60218
sanj1234567898 <casha.shura@gmail.com>
2023-01-22 22:38:06 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $('.bike__slider, .slider__items').slick({
+        arrows: false,
+        dots: true,
+        fade:true,
+        autoplay: true,
+        autoplaySpeed:3500
+
+    });
+
+});
